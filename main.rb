be1b83228acf94e23acd012506c857972ef1359d
onlykir <kekelev.k@mail.ru>
2020-11-04 22:28:04 +0300

    Add global gitignore

--- main.rb
+++ main.rb
@@ -1,12 +1,3 @@
-if (Gem.win_platform?)
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
 require_relative 'lib/console_interface'
 require_relative 'lib/game'
 
